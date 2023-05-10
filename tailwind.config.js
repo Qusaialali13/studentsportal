cb6b7ba50757f93c254353635deecf7d625a83b2
Qusai Al-Ali <qusaialali01@gmail.com>
2023-05-10 17:56:09 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+colors.primary = "#27233A"
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,13 +7,10 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      ...colors,
     },
+    extend: {},
   },
   plugins: [],
 }
