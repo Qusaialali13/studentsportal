b75dfe9beeeeeb9cf3b52d269db42325856d8b58
Qusai Al-Ali <qusaialali01@gmail.com>
2023-05-17 20:39:38 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -40,6 +40,106 @@ export default function Home() {
               </svg>
             </div>
           </div>
+          <div className="flex items-center justify-end w-full mainCardsShadow rounded-[10px]">
+            <div className="flex w-full items-center justify-end pr-4 relative max-h-[110px]  h-[90px] lg:h-[110px] dark:border-l-[1px] dark:border-y-[1px] rounded-tl-[10px] rounded-bl-[10px] ">
+              <span className="text-[#1B2B3C] dark:text-white text-[22px]">
+                {" "}
+                الإستبيانات
+              </span>
+            </div>
+            <div className="flex  items-center justify-center gradient-bg  h-[90px] lg:h-[110px] w-[40%] lg:w-full max-w-[140px] rounded-tr-[10px] rounded-br-[10px]  text-black">
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="fill-white dark:fill-[#1e2e3f]"
+                height="45"
+                width="45"
+              >
+                <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+              </svg>
+            </div>
+          </div>
+          <div className="flex items-center justify-end w-full mainCardsShadow rounded-[10px]">
+            <div className="flex w-full items-center justify-end pr-4 relative max-h-[110px]  h-[90px] lg:h-[110px] dark:border-l-[1px] dark:border-y-[1px] rounded-tl-[10px] rounded-bl-[10px] ">
+              <span className="text-[#1B2B3C] dark:text-white text-[22px]">
+                {" "}
+                الإستبيانات
+              </span>
+            </div>
+            <div className="flex  items-center justify-center gradient-bg  h-[90px] lg:h-[110px] w-[40%] lg:w-full max-w-[140px] rounded-tr-[10px] rounded-br-[10px]  text-black">
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="fill-white dark:fill-[#1e2e3f]"
+                height="45"
+                width="45"
+              >
+                <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+              </svg>
+            </div>
+          </div>
+          <div className="flex items-center justify-end w-full mainCardsShadow rounded-[10px]">
+            <div className="flex w-full items-center justify-end pr-4 relative max-h-[110px]  h-[90px] lg:h-[110px] dark:border-l-[1px] dark:border-y-[1px] rounded-tl-[10px] rounded-bl-[10px] ">
+              <span className="text-[#1B2B3C] dark:text-white text-[22px]">
+                {" "}
+                الإستبيانات
+              </span>
+            </div>
+            <div className="flex  items-center justify-center gradient-bg  h-[90px] lg:h-[110px] w-[40%] lg:w-full max-w-[140px] rounded-tr-[10px] rounded-br-[10px]  text-black">
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="fill-white dark:fill-[#1e2e3f]"
+                height="45"
+                width="45"
+              >
+                <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+              </svg>
+            </div>
+          </div>
+          <div className="flex items-center justify-end w-full mainCardsShadow rounded-[10px]">
+            <div className="flex w-full items-center justify-end pr-4 relative max-h-[110px]  h-[90px] lg:h-[110px] dark:border-l-[1px] dark:border-y-[1px] rounded-tl-[10px] rounded-bl-[10px] ">
+              <span className="text-[#1B2B3C] dark:text-white text-[22px]">
+                {" "}
+                الإستبيانات
+              </span>
+            </div>
+            <div className="flex  items-center justify-center gradient-bg  h-[90px] lg:h-[110px] w-[40%] lg:w-full max-w-[140px] rounded-tr-[10px] rounded-br-[10px]  text-black">
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="fill-white dark:fill-[#1e2e3f]"
+                height="45"
+                width="45"
+              >
+                <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+              </svg>
+            </div>
+          </div>
+          <div className="flex items-center justify-end w-full mainCardsShadow rounded-[10px]">
+            <div className="flex w-full items-center justify-end pr-4 relative max-h-[110px]  h-[90px] lg:h-[110px] dark:border-l-[1px] dark:border-y-[1px] rounded-tl-[10px] rounded-bl-[10px] ">
+              <span className="text-[#1B2B3C] dark:text-white text-[22px]">
+                {" "}
+                الإستبيانات
+              </span>
+            </div>
+            <div className="flex  items-center justify-center gradient-bg  h-[90px] lg:h-[110px] w-[40%] lg:w-full max-w-[140px] rounded-tr-[10px] rounded-br-[10px]  text-black">
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="fill-white dark:fill-[#1e2e3f]"
+                height="45"
+                width="45"
+              >
+                <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+              </svg>
+            </div>
+          </div>
         </div>
       </div>
     </main>
