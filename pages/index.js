7a8dce5afc32436db413bab67d0f31474d4e2e95
qusai <qusaialali01@gmail.com>
2023-05-24 00:28:48 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -111,12 +111,12 @@ export default function Home() {
               <div className="flex  items-center justify-center gradient-bg  h-[90px] lg:h-[110px] w-[40%] lg:w-full max-w-[140px] rounded-tr-[10px] rounded-br-[10px]  text-black">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 384 512"
                   className="fill-white dark:fill-[#1e2e3f]"
                   height="45"
                   width="45"
+                  viewBox="0 0 512 512"
                 >
-                  <path d="M80 0C44.7 0 16 28.7 16 64V448c0 35.3 28.7 64 64 64H304c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H80zM192 400a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
+                  <path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z" />
                 </svg>
               </div>
             </Link>
